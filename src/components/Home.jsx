3b81d6afdf7085df97fbc9f6d8dc59099e91ca95
Bill Dunn <billrdunn@gmail.com>
2022-07-24 17:12:09 +0100

    Fix scaling of grid with different screen sizes.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,9 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Col, Container, Image, Row } from "react-bootstrap";
-// import SearchBar from "./SearchBar";
-// import ItemCard from "./ItemCard";
-// import { setMonth } from "../reducers/monthReducer";
+
 import "../styles.css";
 import Gallery from "react-grid-gallery";
-// import ClickableImage from "./ClickableImage";
 import { Helmet } from "react-helmet";
 
 function getWindowDimensions() {
@@ -34,11 +31,7 @@ export function useWindowDimensions() {
 }
 
 function Home() {
-  const { height, width } = useWindowDimensions();
-  console.log("height :>> ", height);
-  console.log("width :>> ", width);
-
-  const side = width / 3;
+  const { width } = useWindowDimensions();
 
   const tmp = useSelector((state) => state.imgUrls);
   const imgs = tmp.slice().reverse();
@@ -49,12 +42,70 @@ function Home() {
     return {
       src: img.url,
       thumbnail: img.url,
-      thumbnailWidth: side,
-      thumbnailHeight: side,
+      thumbnailWidth: 30,
+      thumbnailHeight: 150,
       isSelected: false,
       caption: "",
     };
   });
+
+  const thumbnailStyle = () => ({
+    width: "100%",
+    height: "100%",
+  });
+
+  // eslint-disable-next-line consistent-return
+  const tileViewportStyle = () => {
+    // eslint-disable-next-line no-unused-vars
+    const tileWidth = (nTiles, windowWidth) => windowWidth / nTiles - (40 + 4 * (nTiles - 1)) / 2;
+
+    if (width > 299 && width < 400) {
+      return {
+        width: `${width / 2 - 28}px`,
+        height: `${width / 2 - 28}px`,
+      };
+      // eslint-disable-next-line no-else-return
+    } else if (width >= 400 && width < 576) {
+      return {
+        width: `${width / 3 - 20}px`,
+        height: `${width / 3 - 20}px`,
+      };
+    } 
+    else if (width >= 576 && width < 768) {
+      return {
+        width: "119px",
+        height: "119px",
+      };
+    }
+    else if (width >= 768 && width < 992) {
+      return {
+        width: "164px",
+        height: "164px",
+      };
+    }
+    else if (width >= 992 && width < 1200) {
+      return {
+        width: "178px",
+        height: "178px",
+      };
+    }
+    else if (width >= 1200 && width < 1400) {
+      return {
+        width: "214px",
+        height: "214px",
+      };
+    }
+    else if (width >= 1400) {
+      return {
+        width: "250px",
+        height: "250px",
+      };
+    }
+    return {
+      width: "100%",
+      height: "100%",
+    };
+  };
 
   return (
     <div>
@@ -78,10 +129,11 @@ function Home() {
       <Container className="gallery-container">
         {showImgs && (
           <Gallery
-            rowHeight={side}
             images={forGallery}
             enableImageSelection={false}
             showImageCount={false}
+            thumbnailStyle={() => thumbnailStyle()}
+            tileViewportStyle={() => tileViewportStyle()}
           />
         )}
       </Container>
