96ac6826a4ea4121be214ec3043f2c355a1863ef
Bill Dunn <billrdunn@gmail.com>
2022-07-27 11:50:51 +0100

    Prevent scrolling on upload page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
-import { Helmet } from "react-helmet";
 
 import Home from "./components/Home";
 import { initImgs } from "./reducers/imgUrlsReducer";
@@ -17,22 +16,13 @@ function App() {
   }, [dispatch]);
 
   return (
-    <>
-      <Helmet>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no height=device-height"
-        />
-      </Helmet>
-
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/kjd7q2bniv09892inafkjf74hertoqm309fnli3498h3" element={<Upload />} />
-          <Route path="/manage-images" element={<ManageImages />} />
-        </Routes>
-      </div>
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/kjd7q2bniv09892inafkjf74hertoqm309fnli3498h3" element={<Upload />} />
+        <Route path="/manage-images" element={<ManageImages />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Helmet } from "react-helmet";
 import Header from "./Header";
 import UploadComponent from "./UploadComponent";
 import HomePageText from "./HomePageText";
@@ -8,6 +9,12 @@ function Upload() {
   const isUploading = useSelector((state) => state.showUploading);
   return (
     <div>
+      <Helmet>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no height=device-height"
+        />
+      </Helmet>
       <div className="bg-black px-10px pb-10px w-auto h-screen">
         <Header />
         <HomePageText blank={!isUploading} />
