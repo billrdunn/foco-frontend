f5b79550b9daeca7a930390e08012bcf423a6398
Bill Dunn <billrdunn@gmail.com>
2022-07-28 09:06:14 +0100

    Test rendering blank upload page.

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import Header from "./Header";
-import UploadComponent from "./UploadComponent";
-import HomePageText from "./HomePageText";
+// import { useSelector } from "react-redux";
+// import Header from "./Header";
+// import UploadComponent from "./UploadComponent";
+// import HomePageText from "./HomePageText";
 
 function Upload() {
-  const isUploading = useSelector((state) => state.showUploading);
+  // const isUploading = useSelector((state) => state.showUploading);
   return (
-    <div className="bg-black px-10px pb-10px w-auto h-screen">
-      <Header />
+    <div className="bg-blue-300 px-10px pb-10px w-auto h-screen">
+      {/* <Header />
       <HomePageText blank={!isUploading} />
-      <UploadComponent />
+      <UploadComponent /> */}
     </div>
   );
 }
