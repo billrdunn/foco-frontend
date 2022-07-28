85a42b25b2394eb078501de4d49c47019381a750
Bill Dunn <billrdunn@gmail.com>
2022-07-28 09:15:09 +0100

    Try to prevent scroll on page.

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-// import { useSelector } from "react-redux";
-// import Header from "./Header";
-// import UploadComponent from "./UploadComponent";
-// import HomePageText from "./HomePageText";
+import { useSelector } from "react-redux";
+import Header from "./Header";
+import UploadComponent from "./UploadComponent";
+import HomePageText from "./HomePageText";
 
 function Upload() {
-  // const isUploading = useSelector((state) => state.showUploading);
+  const isUploading = useSelector((state) => state.showUploading);
   return (
-    <div className="bg-blue-300 px-10px pb-10px w-auto h-screen">
-      {/* <Header />
-      <HomePageText blank={!isUploading} />
-      <UploadComponent /> */}
+    <div className="flex flex-col bg-blue-300 px-10px pb-10px w-auto h-screen">
+      <div className="flex flex-col bg-green-300 h-full">
+        <Header />
+        <HomePageText blank={!isUploading} />
+        <UploadComponent />
+      </div>
     </div>
   );
 }
