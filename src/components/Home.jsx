ea5cc048c69354c7c6afc5b783a1230831c3b74b
Bill Dunn <billrdunn@gmail.com>
2022-07-28 11:51:52 +0100

    Remove bottom padding from home page.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ import Header from "./Header";
 
 function Home() {
   return (
-    <div className="bg-black px-10px pb-10px w-auto h-screen">
+    <div className="bg-black px-10px w-auto h-screen ">
       <Header/>
       <HomePageText />
       <ImagesGrid />
