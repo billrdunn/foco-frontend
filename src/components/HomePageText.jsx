6d32ce9280b211dfa3d694c0bb9897d011b62a28
Bill Dunn <billrdunn@gmail.com>
2022-07-28 16:18:53 +0100

    Tighten desktop text and edit app name in tab.

--- src/components/HomePageText.jsx
+++ src/components/HomePageText.jsx
@@ -8,12 +8,12 @@ function HomePageText({ blank }) {
         <div className="mob:columns-1 tab:columns-1 desk:columns-2 ">
           <div className="text-white text-center font-domaine">
             01.08.22 — Swift, Shoreditch
-            <br />
+            <span className="block mb-[-2px]"/>
             02.08.22 — Calloh Callay, Shoreditch
           </div>
           <div className="text-white text-center font-domaine">
             04.08.22 — Lab 22, Cardiff
-            <br />
+            <span className="block mb-[-2px]"/>
             07.08.22 — Filthy XIII, Bristol
           </div>
         </div>
