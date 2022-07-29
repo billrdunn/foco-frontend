68500bf9e706d1cf4c5ea7b38ad26f52d9d7f669
Bill Dunn <billrdunn@gmail.com>
2022-07-29 14:34:48 +0100

    Add insta and email links.

--- src/components/HomePageText.jsx
+++ src/components/HomePageText.jsx
@@ -4,20 +4,31 @@ import React from "react";
 function HomePageText({ blank }) {
   if (!blank) {
     return (
-      <div className="h-auto align-top w-auto justify-center ">
-        <div className="mob:columns-1 tab:columns-1 desk:columns-2 ">
-          <div className="text-white text-center font-domaine">
-            01.08.22 — Swift, Shoreditch
-            <span className="block mb-[-2px]"/>
-            02.08.22 — Calloh Callay, Shoreditch
-          </div>
-          <div className="text-white text-center font-domaine">
-            04.08.22 — Lab 22, Cardiff
-            <span className="block mb-[-2px]"/>
-            07.08.22 — Filthy XIII, Bristol
+      <>
+        <div className="h-auto align-top w-auto justify-center ">
+          <div className="mob:columns-1 tab:columns-1 desk:columns-2 ">
+            <div className="text-white text-center font-domaine">
+              01.08.22 — Swift, Shoreditch
+              <span className="block mb-[-2px]" />
+              02.08.22 — Calloh Callay, Shoreditch
+            </div>
+            <div className="text-white text-center font-domaine">
+              04.08.22 — Lab 22, Cardiff
+              <span className="block mb-[-2px]" />
+              07.08.22 — Filthy XIII, Bristol
+            </div>
           </div>
         </div>
-      </div>
+        <div className="h-auto align-top w-auto justify-center  pt-[60px]">
+          <div className="mob:columns-1 tab:columns-1 desk:columns-2 ">
+            <div className="text-white text-center font-domaine">
+              <a href="https://www.instagram.com/foco.bcn/">Instagram</a>
+              <span className="block mb-[-2px]" />
+              <a href="mailto:info@focobcn.com">Email</a>
+            </div>
+          </div>
+        </div>
+      </>
     );
   }
   return (
