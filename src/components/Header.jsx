8c7b0d419b1a8a7bb477ee2e973db4136289a563
Bill Dunn <billrdunn@gmail.com>
2022-07-29 16:14:02 +0100

    Style email and insta links.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Logo from "./Logo";
 
 function Header() {
+  // eslint-disable-next-line no-unused-vars
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+    });
+  }, []);
+
+  if (window.innerWidth < 1024) {
+    return (
+      <div className="flex place-content-evenly mob:columns-1 tab:columns-1 desk:columns-3 py-[60px]">
+        <div className="h-auto mx-[-88px] logo items-end ">
+          <Logo />
+        </div>
+      </div>
+    );
+  }
   return (
-    <div className="flex  w-auto h-auto items-end pt-[60px] mb-[60px] ">
-      <div className="flex h-auto logo mx-auto items-end ">
+    <div className="flex place-content-evenly mob:columns-1 tab:columns-1 desk:columns-3 py-[60px]">
+      <div className="flex items-center h-auto logo ">
+        <div className=" h-auto w-autologo mx-auto items-end text-white font-domaine">
+          <a href="mailto:info@focobcn.com">Email</a>
+        </div>
+      </div>
+      <div className="h-auto mx-[-88px] logo items-end ">
         <Logo />
+      </div>
+      <div className="flex h-auto logo items-center ">
+        <div className=" text-white w-autologo mx-auto items-end b  font-domaine">
+          <a href="https://www.instagram.com/foco.bcn/" target="_blank" rel="noreferrer noopener">
+            Instagram
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/components/HomePageText.jsx
+++ src/components/HomePageText.jsx
@@ -1,34 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // eslint-disable-next-line react/prop-types
 function HomePageText({ blank }) {
+  // eslint-disable-next-line no-unused-vars
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+    });
+  }, []);
+
   if (!blank) {
+    if (window.innerWidth < 1024) {
+      return (
+        <>
+          <div className="h-auto align-top w-auto justify-center ">
+            <div className="mob:columns-1 tab:columns-1 desk:columns-2 ">
+              <div className="text-white text-center font-domaine">
+                01.08.22 — Swift, Shoreditch
+                <span className="block mb-[-2px]" />
+                02.08.22 — Calloh Callay, Shoreditch
+              </div>
+              <div className="text-white text-center font-domaine">
+                04.08.22 — Lab 22, Cardiff
+                <span className="block mb-[-2px]" />
+                07.08.22 — Filthy XIII, Bristol
+              </div>
+            </div>
+          </div>
+          <div className="h-auto align-top w-auto justify-center  pt-[60px]">
+            <div className="mob:columns-1 tab:columns-1 desk:columns-2 ">
+              <div className="text-white text-center font-domaine">
+                <a
+                  href="https://www.instagram.com/foco.bcn/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  Instagram
+                </a>
+                <span className="block mb-[-2px]" />
+                <a href="mailto:info@focobcn.com">Email</a>
+              </div>
+            </div>
+          </div>
+        </>
+      );
+    }
+    // desktop view
     return (
-      <>
-        <div className="h-auto align-top w-auto justify-center ">
-          <div className="mob:columns-1 tab:columns-1 desk:columns-2 ">
-            <div className="text-white text-center font-domaine">
-              01.08.22 — Swift, Shoreditch
-              <span className="block mb-[-2px]" />
-              02.08.22 — Calloh Callay, Shoreditch
-            </div>
-            <div className="text-white text-center font-domaine">
-              04.08.22 — Lab 22, Cardiff
-              <span className="block mb-[-2px]" />
-              07.08.22 — Filthy XIII, Bristol
-            </div>
+      <div className="h-auto align-top w-auto justify-center ">
+        <div className="mob:columns-1 tab:columns-1 desk:columns-2 ">
+          <div className="text-white text-center font-domaine">
+            01.08.22 — Swift, Shoreditch
+            <span className="block mb-[-2px]" />
+            02.08.22 — Calloh Callay, Shoreditch
+          </div>
+          <div className="text-white text-center font-domaine">
+            04.08.22 — Lab 22, Cardiff
+            <span className="block mb-[-2px]" />
+            07.08.22 — Filthy XIII, Bristol
           </div>
         </div>
-        <div className="h-auto align-top w-auto justify-center  pt-[60px]">
-          <div className="mob:columns-1 tab:columns-1 desk:columns-2 ">
-            <div className="text-white text-center font-domaine">
-              <a href="https://www.instagram.com/foco.bcn/">Instagram</a>
-              <span className="block mb-[-2px]" />
-              <a href="mailto:info@focobcn.com">Email</a>
-            </div>
-          </div>
-        </div>
-      </>
+      </div>
     );
   }
   return (
