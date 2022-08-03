f3f5673229c0590f5ab23980d01f3c50fd9db936
Bill Dunn <billrdunn@gmail.com>
2022-08-03 11:58:05 +0100

    Change logo to svg.

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Logo() {
   return (
     // <div className="logo mx-auto items-center">
-    <img src="https://focobcn.s3.eu-west-3.amazonaws.com/FOCO_LOGO.png" alt="logo" />
+    <img src="https://focobcn.s3.eu-west-3.amazonaws.com/FOCO_LOGO.svg" alt="logo" />
   );
 }
 
